69a8b33beedc967b630e54f9c2acead28fd950c0
PiyumG <pg.piyum.guruge@gmail.com>
2022-03-26 05:54:46 +0530

    init

---
+++ src/test/java/com/hcl/hack/hackathon/HackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.hack.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
