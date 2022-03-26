ccdb3105b217cf0f40c28dc211cf58ce69450af2
PiyumG <pg.piyum.guruge@gmail.com>
2022-03-26 06:57:05 +0530

    config more

---
+++ src/main/java/com/hcl/hack/hackathon/Controller/ApplicationController.java
@@ -0,0 +1,12 @@
+package com.hcl.hack.hackathon.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hackothon")
+public class ApplicationController {
+
+
+
+}
